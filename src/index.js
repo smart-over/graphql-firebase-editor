a4270a5f1118332c6ce6201e70ff281b78d3c76f
Ömer Kamçılı <okamcili@smartover.net>
2019-09-24 10:16:01 +0300

    App name fetched from environment variable

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 const graphqlEditor = async function (req, res, next){
     fs.readFile(__dirname + '/graphi.html', 'utf8', (err, text) => {
         text = text.replace('FIREBASE_KEY', process.env.FIREBASE_KEY)
+        text = text.replace('APP_NAME', process.env.APP_NAME || 'GraphQL Editor')
         res.send(text);
     })
 }
